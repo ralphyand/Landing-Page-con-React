c61ddd9a8f8bbd22ea78e4aed6d4da8e59b76708
ralphyand <MOISELK211@GMAIL.COM>
2022-06-13 14:33:33 +0000

    arreglado

--- src/js/component/donw.jsx
+++ src/js/component/donw.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Donw = () => {
 	return (
 		<div className=" text-center p-3 mt-3 bg-secondary text-white bg-dark">
-			<p className="text">Todos los derechos reservados</p>
+			<p className="text">Copyright © Your website 2021</p>
 		</div>
 	);
 };
